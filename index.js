6ad1f926105b2cf0e13d4f16ed5dcd1720f9a783
Amber Robson <amberrobson95@gmail.com>
2022-08-08 20:58:51 +0100

    added styling and js to increment count

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function increment(value) {
-  console.log(value);
+let homeScore = document.getElementById("home-score");
+
+let guestScore = document.getElementById("guest-score");
+
+let hScore = 0;
+
+let gScore = 0;
+
+function homeIncrement(value) {
+  hScore = hScore + value;
+  homeScore.textContent = hScore;
+}
+
+function guestIncrement(value) {
+  gScore = gScore + value;
+  guestScore.textContent = gScore;
 }
